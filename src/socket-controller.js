da5a5ae3e487fd081555af424f1a72ff123d4051
dewantara <dewantara.tirta@gmail.com>
2024-11-10 18:14:00 +0700

    feat(update chat)

--- src/socket-controller.js
+++ src/socket-controller.js
@@ -1,9 +1,20 @@
 // create controller
 module.exports = (socket, io) => {
 
-    socket.on('chat message', (data) => {
-        console.log(`${data.name}: ${data.msg}`);
-        io.emit('chat message', data);
+
+    socket.on('chat message', (message) => {
+        io.emit('chat message', {
+            ...message,
+            userId: socket.id
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+
+    socket.on('error', (error) => {
+        console.error('Socket error:', error);
     });
 
 };
